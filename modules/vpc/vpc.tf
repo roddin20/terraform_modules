3d509924c281ad8c9d3fc42bb6fcd459578b6c0a
Rob Russo <robert.russo@hbo.com>
2016-09-22 16:53:59 -0400

    Adding default route creation

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -15,9 +15,24 @@ resource "aws_internet_gateway" "igw" {
     }
 }
 
+resource "aws_route_table" "default-route-table" {
+    vpc_id = "${aws_vpc.vpc.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.igw.id}"
+    }
+    depends_on = ["aws_vpc.vpc", "aws_internet_gateway.igw"]
+    tags {
+        Name = "${var.app_name}-default-route-table}"
+    }
+}
+
 output "vpc_id" {
     value = "${aws_vpc.vpc.id}"
 }
 output "igw_id" {
     value = "${aws_internet_gateway.igw.id}"
+}
+output "default-tour-table_id" {
+    value = "${aws_route_table.default-route-table.id}"
 }
