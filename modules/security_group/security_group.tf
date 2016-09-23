45cd44e28b07c1459072bd281e275dce71a18af6
Rob Russo <robert.russo@hbo.com>
2016-09-23 14:30:06 -0400

    Adding split for cidr

--- modules/security_group/security_group.tf
+++ modules/security_group/security_group.tf
@@ -14,7 +14,7 @@ resource "aws_security_group_rule" "sg_rules" {
     from_port = "${lookup(var.sg_rule_from_ports, count.index)}"
     to_port = "${lookup(var.sg_rule_to_ports, count.index)}"
     protocol = "${lookup(var.sg_rule_protocols, count.index)}"
-    cidr_blocks = "${lookup(var.sg_rule_cidr_blocks, count.index)}"
+    cidr_blocks = "${split(",", lookup(var.sg_rule_cidr_blocks, count.index))}"
 
     security_group_id = "${aws_security_group.security_group.id}"
 }
