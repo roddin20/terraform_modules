399adaa88e0615a4b4afed1fcb39338741533d39
Rob Russo <robert.russo@hbo.com>
2016-09-23 17:12:33 -0400

    Adding variables for instance modules

---
+++ modules/compute/variables.tf
@@ -0,0 +1,15 @@
+variable "app_name" {}
+variable "instance_name" {}
+variable "role" {}
+
+variable "instance_count" {
+    default = "1"
+}
+
+variable "ami" {}
+variable "instance_type" {}
+
+variable "subnet_id" {}
+variable "vpc_security_group_ids" { type = "list" }
+
+variable "user_data" {}
