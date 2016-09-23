5bece8619b4a9c5cecd98f7f7481873ac62a1f30
Rob Russo <robert.russo@hbo.com>
2016-09-23 10:20:31 -0400

    Adding security_group module

---
+++ modules/security_group/security_group.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group" "security_group" {
+    name = "${var.name}"
+    description = "${var.description}"
+    vpc_id = "${var.vpc_id}"
+
+    tags {
+        Name = "${var.app_name}-sg-${var.name}"
+    }
+}
---
+++ modules/security_group/variables.tf
@@ -0,0 +1,5 @@
+variable "app_name" {}
+variable "description" {}
+variable "name" {}
+
+variable "vpc_id" {}
